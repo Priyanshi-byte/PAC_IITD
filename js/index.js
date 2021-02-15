5a2d31a770e6262a045bc79cb8bb60d5f1140806
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2021-02-15 20:06:56 +0530

    preloader

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
+// $(window).load(function () {
+// 	// will first fade out the loading animation
+// 	$('#loader').fadeOut('slow', function () {
+// 		// will fade out the whole DIV that covers the website.
+// 		$('#preloader').delay(300).fadeOut('slow');
+// 		$('body').delay(350).css({ overflow: 'visible' });
+// 	});
+// 	$('body').delay(350).css({ overflow: 'visible' });
+// });
+
+$(window).on('load', function () {
+	// makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({ overflow: 'visible' });
+});
 var $headline = $('.headline'),
-    $inner = $('.inner'),
-    $nav = $('nav'),
-    navHeight = 75;
+	$inner = $('.inner'),
+	$nav = $('nav'),
+	navHeight = 75;
 
-$(window).scroll(function() {
-  var scrollTop = $(this).scrollTop(),
-      headlineHeight = $headline.outerHeight() - navHeight,
-      navOffset = $nav.offset().top;
+$(window).scroll(function () {
+	var scrollTop = $(this).scrollTop(),
+		headlineHeight = $headline.outerHeight() - navHeight,
+		navOffset = $nav.offset().top;
 
-  $headline.css({
-    'opacity': (1 - scrollTop / headlineHeight)
-  });
-  $inner.children().css({
-    'transform': 'translateY('+ scrollTop * 0.4 +'px)',
-  });
-  if (navOffset > headlineHeight) {
-    $nav.addClass('scrolled');
-  } else {
-    $nav.removeClass('scrolled');
-  }
+	$headline.css({
+		opacity: 1 - scrollTop / headlineHeight,
+	});
+	$inner.children().css({
+		transform: 'translateY(' + scrollTop * 0.4 + 'px)',
+	});
+	if (navOffset > headlineHeight) {
+		$nav.addClass('scrolled');
+	} else {
+		$nav.removeClass('scrolled');
+	}
 });
